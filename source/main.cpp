7a84b07f117bbf5b84425bc81b98f3040ed24d13
uwe <40294263+UweSilver@users.noreply.github.com>
2023-08-30 12:18:29 +0900

    clang formatを追加

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,13 @@
+#include <GLFW/glfw3.h>
+#include <webgpu/webgpu_cpp.h>
+
 #include <iostream>
 #include <memory>
 
-#include <webgpu/webgpu_cpp.h>
-#include <GLFW/glfw3.h>
-
 #if defined(__EMSCRIPTEN__)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #else
-    #include <webgpu/webgpu_glfw.h>
+#include <webgpu/webgpu_glfw.h>
 #endif
 
 wgpu::Instance instance = nullptr;
@@ -16,35 +16,34 @@ wgpu::Device device = nullptr;
 const uint32_t k_width = 512;
 const uint32_t k_height = 512;
 
-void start(){
-    
-}
+void start() {}
 
-int main(){
-    instance = wgpu::CreateInstance();
+int main() {
+  instance = wgpu::CreateInstance();
 
-    if(!glfwInit()){
-        return 1;
-    }
+  if (!glfwInit()) {
+    return 1;
+  }
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window =glfwCreateWindow(k_width, k_height, "WebGPU window", nullptr, nullptr);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window =
+      glfwCreateWindow(k_width, k_height, "WebGPU window", nullptr, nullptr);
 
-    #if defined(__EMSCRIPTEN__)
-        wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
-        canvasDesc.selector = "#canvas";
+#if defined(__EMSCRIPTEN__)
+  wgpu::SurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
+  canvasDesc.selector = "#canvas";
 
-        wgpu::SurfaceDescriptor surfaceDesc{.nextInChain = &canvasDesc};
-        wgpu::Surface surface = instance.CreateSurface(&surfaceDesc);
-    #else
-        wgpu::Surface surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
-    #endif
+  wgpu::SurfaceDescriptor surfaceDesc{.nextInChain = &canvasDesc};
+  wgpu::Surface surface = instance.CreateSurface(&surfaceDesc);
+#else
+  wgpu::Surface surface = wgpu::glfw::CreateSurfaceForWindow(instance, window);
+#endif
 
-    #if defined(__EMSCRIPTEN__)
-        //emscripten_set_main_loop(Render)
-    #else
-        while(!glfwWindowShouldClose(window)){
-            glfwPollEvents();
-        }
-    #endif
+#if defined(__EMSCRIPTEN__)
+  // emscripten_set_main_loop(Render)
+#else
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+#endif
 }
