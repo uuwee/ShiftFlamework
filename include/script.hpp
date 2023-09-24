24a88686610b839fce31d684aa4902a54dfd304d
uwe <40294263+UweSilver@users.noreply.github.com>
2023-09-24 15:07:40 +0900

    VS向けのコードを隔離する

--- include/script.hpp
+++ include/script.hpp
@@ -17,12 +17,14 @@ class Script : public Component {
   Script(std::shared_ptr<Entity> parent) : Component(parent) {}
 
   void load_dll() {
+#if defined(_MSC_VER)
     HMODULE module = LoadLibrary(
         "../../../sample/script_import/out/win/Debug/test_script.dll");
     if (module == NULL) return;
 
     action func1 = (action)GetProcAddress(module, "on_update");
     func1();
+#endif
   }
   void run() {}
 };
