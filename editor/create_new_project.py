8742cf21d02b77aef5fd6a47978a05d813d06e1c
uwe <40294263+UweSilver@users.noreply.github.com>
2023-10-15 18:29:55 +0900

    新しいプロジェクトを作成できるようにする（Winのみ）

---
+++ editor/create_new_project.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import shutil
+
+path_to_new_proj = sys.argv[1]
+proj_name = sys.argv[2]
+new_proj_path = os.path.join(path_to_new_proj, proj_name)
+
+# cmake
+os.makedirs(new_proj_path, exist_ok=True)
+os.makedirs(os.path.join(new_proj_path,"include"), exist_ok=True)
+os.makedirs(os.path.join(new_proj_path, "source"), exist_ok=True)
+with open(os.path.join(new_proj_path, "CmakeLists.txt"), mode="x", encoding="utf-8") as cmake:
+    cmake.writelines(["cmake_minimum_required(VERSION 3.26)\n",
+                      "project(" + proj_name +")\n",
+                      "set(CMAKE_CXX_STANDARD 20)\n",
+                      "file(GLOB includes include/*.hpp)\n",
+                      "file(GLOB sources source/*.cpp)\n",
+                      "add_library(" + proj_name + " SHARED ${sources} ${includes})\n",
+                      "target_compile_features(" + proj_name + " PRIVATE cxx_std_20)\n",
+                      "target_include_directories(" + proj_name + " PUBLIC include include/ShiftFlamework)\n"])
+    pass
+
+# gitignore
+shutil.copyfile(os.path.dirname(__file__) + "/new_project/.gitignore", path_to_new_proj + "/" + proj_name + "/.gitignore")
+
+# headers
+shutil.copytree(os.path.dirname(os.path.dirname(__file__)) + "/include", path_to_new_proj + "/" + proj_name + "/include/ShiftFlamework")
