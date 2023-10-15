cc555dcb42d7c0ab4d999c7fe21cbf002451bd5f
uwe <40294263+UweSilver@users.noreply.github.com>
2023-10-15 18:36:57 +0900

    プロジェクト生成スクリプトを生成

--- editor/create_new_project.py
+++ editor/create_new_project.py
@@ -5,6 +5,8 @@ import shutil
 path_to_new_proj = sys.argv[1]
 proj_name = sys.argv[2]
 new_proj_path = os.path.join(path_to_new_proj, proj_name)
+editor_path = os.path.dirname(__file__)
+editor_resource_path = os.path.join(editor_path, "new_project")
 
 # cmake
 os.makedirs(new_proj_path, exist_ok=True)
@@ -22,7 +24,7 @@ with open(os.path.join(new_proj_path, "CmakeLists.txt"), mode="x", encoding="utf
     pass
 
 # gitignore
-shutil.copyfile(os.path.dirname(__file__) + "/new_project/.gitignore", path_to_new_proj + "/" + proj_name + "/.gitignore")
+shutil.copyfile(os.path.join(editor_resource_path, ".gitignore"), os.path.join(new_proj_path, ".gitignore"))
 
 # headers
 shutil.copytree(os.path.dirname(os.path.dirname(__file__)) + "/include", path_to_new_proj + "/" + proj_name + "/include/ShiftFlamework")
