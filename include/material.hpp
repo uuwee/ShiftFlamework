a85719584e586d465ef9f8064eacb6d22de0a444
uwe <40294263+UweSilver@users.noreply.github.com>
2023-10-31 13:43:54 +0900

    テクスチャをマテリアルに持たせる

--- include/material.hpp
+++ include/material.hpp
@@ -8,8 +8,8 @@ class Material : public Component {
  public:
   wgpu::Texture texture = nullptr;
   wgpu::TextureView texture_view = nullptr;
+  wgpu::Sampler sampler = nullptr;
   wgpu::BindGroup bindgroup = nullptr;
-  void create_gpu_buffer();
-  void update_gpu_buffer();
+  void create_gpu_buffer(uint32_t height, uint32_t width, const uint8_t *data);
 };
 }  // namespace ShiftFlamework
