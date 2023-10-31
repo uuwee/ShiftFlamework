6e833ea95a90ad04a6b8c37ba57e29824499638c
uwe <40294263+UweSilver@users.noreply.github.com>
2023-10-31 13:10:42 +0900

    定数バッファをTransform Componentに移動する

--- include/screenspace_transform.hpp
+++ include/screenspace_transform.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "entity.hpp"
+#include "graphics.hpp"
 #include "vector.hpp"
 
 namespace ShiftFlamework {
@@ -11,5 +12,10 @@ class ScreenSpaceTransform : public Component {
   float angle = 0;
   Math::Vector2f scale = Math::Vector2f({1, 1});
   uint32_t z_order = 0;  // min comes foreground
+  wgpu::Buffer constant_buffer = nullptr;
+  wgpu::BindGroup bindgroup = nullptr;
+  void create_gpu_buffer();
+  void update_gpu_buffer();
+  void on_register();
 };
 }  // namespace ShiftFlamework
