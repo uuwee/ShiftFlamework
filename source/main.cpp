6a623afe1a6506175645999dc064b6f005663ff4
uwe <40294263+UweSilver@users.noreply.github.com>
2023-11-01 14:30:14 +0900

    ゲームコードを削除

--- source/main.cpp
+++ source/main.cpp
@@ -12,28 +12,8 @@
 #include "vector.hpp"
 using namespace ShiftFlamework;
 
-std::vector<std::shared_ptr<Entity>> pool{};
-
-void spawn_boal()
-{
-  auto boal = std::make_shared<Entity>();
-  boal->add_component<ScreenSpaceMesh>();
-  auto transform = boal->add_component<ScreenSpaceTransform>();
-  transform->position = Math::Vector2f({0, 0.8});
-  transform->scale = Math::Vector2f({0.25, 0.25});
-  boal->add_component<Material>()->create_gpu_buffer(
-      test_image_width, test_image_height, test_image_data);
-  boal->add_component<RigidBody>()->radius =
-      boal->get_component<ScreenSpaceTransform>()->scale.internal_data[0] * 0.5f;
-  pool.push_back(boal);
-}
-
 void main_loop() {
   // user script
-  if (Engine::get_module<Input>()->get_keyboard_state(Keyboard::W) ==
-      ButtonState::DOWN) {
-    spawn_boal();
-  }
 
   Engine::get_module<ScreenSpacePhysics>()->update();
   Engine::get_module<Input>()->update();
@@ -58,7 +38,6 @@ void start() {
   Engine::get_module<ScreenSpaceMeshRenderer>()->initialize();
 
   // game initialize
-  spawn_boal();
 
   // start main loop
   Engine::get_module<Window>()->start_main_loop(main_loop);
