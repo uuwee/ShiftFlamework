4f4a1a1f85215be8409ff6b8aba6bf20ab485d59
uwe <40294263+UweSilver@users.noreply.github.com>
2023-11-02 13:20:31 +0900

    物理シミュレーションの加速度を修正

--- source/screenspace_physics.cpp
+++ source/screenspace_physics.cpp
@@ -22,7 +22,7 @@ void ScreenSpacePhysics::update() {
           body->entity->get_component<ScreenSpaceTransform>()->position;
 
       // gravity
-      //if (old_position.internal_data[1] - body->radius > -1.0f + 0.001f)
+      // if (old_position.internal_data[1] - body->radius > -1.0f + 0.001f)
       body->velocity += body->gravity;
 
       // collide with wall
@@ -62,23 +62,28 @@ void ScreenSpacePhysics::update() {
           const auto pos_diff =
               (old_position + body->velocity) -
               (other_body->entity->get_component<ScreenSpaceTransform>()
-                   ->position);
-          
+                   ->position +
+               other_body->velocity);
+
           if (Math::Length(pos_diff) < (body->radius + other_body->radius)) {
             body->velocity -= body->gravity;
             other_body->velocity -= other_body->gravity;
             const auto normal = Math::GetNormalized(pos_diff);
-            body->velocity += -2.0 * Math::Dot(body->velocity, normal) * body->velocity * body->restitution * 0.1;
-            other_body->velocity += +2.0 * Math::Dot(body->velocity, normal) *
-                                    other_body->velocity *
-                                    other_body->restitution * 0.1;
+            body->velocity += -2.0 * Math::Dot(body->velocity, normal) *
+                              body->velocity * body->restitution;
+            other_body->velocity +=
+                -2.0 * Math::Dot(other_body->velocity, -normal) *
+                other_body->velocity * other_body->restitution;
 
             body->entity->get_component<ScreenSpaceTransform>()->position +=
                 -Math::GetNormalized(pos_diff) *
-                (body->radius + other_body->radius - Math::Length(pos_diff)) * 0.5 * 1.1;
-            other_body->entity->get_component<ScreenSpaceTransform>()->position +=
+                (body->radius + other_body->radius - Math::Length(pos_diff)) *
+                0.5 * 1.1;
+            other_body->entity->get_component<ScreenSpaceTransform>()
+                ->position +=
                 Math::GetNormalized(pos_diff) *
-                (body->radius + other_body->radius - Math::Length(pos_diff)) * 0.5 * 1.1;
+                (body->radius + other_body->radius - Math::Length(pos_diff)) *
+                0.5 * 1.1;
 
             old_position =
                 body->entity->get_component<ScreenSpaceTransform>()->position;
@@ -99,11 +104,13 @@ void ScreenSpacePhysics::update() {
           std::min(1.0f - body->radius, transform->position.internal_data[1]);
       transform->position.internal_data[1] =
           std::max(-1.0f + body->radius, transform->position.internal_data[1]);
-      if (transform->position.internal_data[1] != transform->position.internal_data[1]) {
+      if (transform->position.internal_data[1] !=
+          transform->position.internal_data[1]) {
         body->velocity += body->gravity;
       }
 
-      body->velocity = (transform->position - old_position) * body->deceleration;
+      body->velocity =
+          (transform->position - old_position) * body->deceleration;
     }
   }
 }
