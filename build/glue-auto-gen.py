66fa93663908b35035c20e78512fd8e150a2ae66
uwe <40294263+UweSilver@users.noreply.github.com>
2024-03-14 21:40:34 +0900

    プロパティのゲッター、セッターの実装、出力コードの修正

--- build/glue-auto-gen.py
+++ build/glue-auto-gen.py
@@ -27,6 +27,7 @@ def get_class_tree(input_filenames):
                     "namespace": namespace.copy(),
                     "base_classes": [base.spelling for base in cursor.get_children() if base.kind == CursorKind.CXX_BASE_SPECIFIER],
                     "methods": {},
+                    "properties": {},
                     "constructors": [method.spelling for method in cursor.get_children() if method.kind == CursorKind.CONSTRUCTOR],
                     "destructor": [method.spelling for method in cursor.get_children() if method.kind == CursorKind.DESTRUCTOR],
                 }
@@ -42,6 +43,14 @@ def get_class_tree(input_filenames):
                     for c in method.get_children():
                         if c.kind == CursorKind.PARM_DECL:
                             classes[cursor.spelling]["methods"][method.spelling]["parameters"][c.spelling] = c.type.spelling
+
+                for property in cursor.get_children():
+                    if not property.kind == CursorKind.FIELD_DECL:
+                        continue
+                    classes[cursor.spelling]["properties"][property.spelling] = {
+                        "return_type": property.type.spelling,
+                        "access_specifier": property.access_specifier.name,
+                    }
 
             if cursor.kind == CursorKind.TYPE_ALIAS_DECL:
                 typealiases[cursor.spelling] = {
@@ -99,6 +108,11 @@ def generate_glue_code(class_tree):
         perfect_class_name += namespace + "_"
     perfect_class_name += name
 
+    cpp_perfect_class_name = ""
+    for namespace in tree["namespace"]:
+        cpp_perfect_class_name += namespace + "::"
+    cpp_perfect_class_name += name
+
     code = ""
     # constructor
     # EXPORT void* ShiftFlamework_ExampleClass_Construcotor() {
@@ -107,7 +121,7 @@ def generate_glue_code(class_tree):
     #     return ptr;
     # }
     code += f"EXPORT void* {perfect_class_name}_Constructor() {{\n"
-    code += f"    auto ptr = new {perfect_class_name}();\n"
+    code += f"    auto ptr = new {cpp_perfect_class_name}();\n"
     code += f"    ptr->add_reference();\n"
     code += f"    return ptr;\n"
     code += "}\n\n"
@@ -118,7 +132,7 @@ def generate_glue_code(class_tree):
     #     obj->remove_reference();
     # }
     code += f"EXPORT void {perfect_class_name}_Destructor(void* self) {{\n"
-    code += f"    auto obj = ({perfect_class_name}*)self;\n"
+    code += f"    auto obj = ({cpp_perfect_class_name}*)self;\n"
     code += f"    obj->remove_reference();\n"
     code += "}\n\n"
 
@@ -134,19 +148,40 @@ def generate_glue_code(class_tree):
         code += f"EXPORT {method['return_type']} {perfect_class_name}_{method_name}(void* self"
         for param_name, param_type in method["parameters"].items():
             code += f", {param_type} {param_name}"
-        code += ") {{\n"
-        code += f"    auto obj = ({perfect_class_name}*)self;\n"
+        code += ") {\n"
+        code += f"    auto obj = ({cpp_perfect_class_name}*)self;\n"
         code += f"    obj->{method_name}("
         if len(method["parameters"]) > 0:
             code += f"{', '.join([param_name for param_name in method['parameters']])}"
         code += ");\n"
         code += "}\n\n"
-
-
         pass
 
     # properties
-    print(code)
+    for property_name, property in tree["properties"].items():
+        # public only
+        if property["access_specifier"] != "PUBLIC":
+            continue
+        # EXPORT void ShiftFlamework_ExampleClass_Property_Setter(void* self, const int value) {
+        #     auto obj = (ShiftFlamework::ExampleClass*)self;
+        #     obj->property = value;
+        # }
+        code += f"EXPORT void {perfect_class_name}_{property_name}_Setter(void* self, {property['return_type']} value) {{\n"
+        code += f"    auto obj = ({cpp_perfect_class_name}*)self;\n"
+        code += f"    obj->{property_name} = value;\n"
+        code += "}\n\n"
+
+        # EXPORT int ShiftFlamework_ExampleClass_Property_Getter(void* self) {
+        #     auto obj = (ShiftFlamework::ExampleClass*)self;
+        #     return obj->property;
+        # }
+        code += f"EXPORT {property['return_type']} {perfect_class_name}_{property_name}_Getter(void* self) {{\n"
+        code += f"    auto obj = ({cpp_perfect_class_name}*)self;\n"
+        code += f"    return obj->{property_name};\n"
+        code += "}\n\n"
+
+    # print(code)
+    return code
 
 if __name__ == '__main__':
     print("Running glue-auto-gen.py")
@@ -154,6 +189,7 @@ if __name__ == '__main__':
     # configure clang path
     parser = argparse.ArgumentParser()
     parser.add_argument("--clang-path", type=str , help="Path to clang", required=True)
+    parser.add_argument("--output", type=str , help="Output file", required=True)
     args = parser.parse_args()
 
     clang.cindex.Config.set_library_path(args.clang_path)
@@ -166,5 +202,15 @@ if __name__ == '__main__':
     export_classes = enumerate_all_export_classes(headers)
 
     print("Generating glue code")
+    code = ""
+    # includes
+    code += f"#include <memory>\n"
+    for headers in headers:
+        code += f"#include \"{headers}\"\n"
+
     for k, v in export_classes.items():
-        generate_glue_code((k, v))
+        code += generate_glue_code((k, v))
+
+    # write to file
+    with open(args.output, "w") as f:
+        f.write(code)
