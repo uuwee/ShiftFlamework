6a4030a75b124e37ee31f0a283500ac7bd9c7eac
uwe <40294263+UweSilver@users.noreply.github.com>
2024-06-21 21:07:45 +0900

    wip

--- runtime/source/script.cpp
+++ runtime/source/script.cpp
@@ -19,8 +19,7 @@ extern "C" __declspec(dllexport) void on_start() {
   e = create_entity();
   e->add_component<ScreenSpaceMesh>();
   e->add_component<ScreenSpaceTransform>();
-  auto mat = e->add_component<Material>();
-  e->get_component<Material>()->create_gpu_buffer(
+  e->add_component<Material>()->create_gpu_buffer(
       test_image_height, test_image_width, test_image_data);
 }
 
