355015af075d65d9c0de56c571b58f63317472d0
uwe <40294263+UweSilver@users.noreply.github.com>
2024-07-25 10:58:33 +0900

    wip dont work

--- source/main.cpp
+++ source/main.cpp
@@ -31,7 +31,7 @@ using namespace SF;
 
 void import() {
   const auto filePath =
-      "E:/resources/models/Bistro_v5_2/Bistro_v5_2/BistroExterior.fbx";
+      "D:/resources/models/Bistro_v5_2/Bistro_v5_2/BistroExterior.fbx";
   Assimp::Importer importer;
   const aiScene* scene = importer.ReadFile(
       filePath, aiProcess_Triangulate | aiProcess_CalcTangentSpace |
@@ -109,7 +109,7 @@ void import() {
         auto pos = std::string(name.C_Str()).find_last_of("\\");
         auto len = 1;
         auto path = std::filesystem::directory_entry(
-                        "E:/resources/models/Bistro_v5_2/Bistro_v5_2/" +
+                        "D:/resources/models/Bistro_v5_2/Bistro_v5_2/" +
                         std::string(name.C_Str()).replace(pos, len, "/"))
                         .path();
 
