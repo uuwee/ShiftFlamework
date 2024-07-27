e7fcd5a2f5afa34485ac7cce9d61f5d29964f94d
uwe <40294263+UweSilver@users.noreply.github.com>
2024-07-27 12:25:48 +0900

    wip

--- include/dds_loader.hpp
+++ include/dds_loader.hpp
@@ -1,15 +1,81 @@
 #pragma once
 #include <stdint.h>
 
-#include <filesystem>
-#include <vector>
 #include <array>
 #include <bitset>
+#include <filesystem>
+#include <vector>
 
 namespace SF::DDSLoader {
-struct DDSHeader{
+
+enum class DDSFlags : uint32_t {
+  caps = 0x00000001,
+  height = 0x00000002,
+  width = 0x00000004,
+  pitch = 0x00000008,
+  pixel_format = 0x00001000,
+  mip_map_count = 0x00020000,
+  linear_size = 0x00080000,
+  depth = 0x00800000,
+};
+
+std::vector<std::string> dump_flags(uint32_t flags);
+
+enum class DDSPixelFormatFlags {
+  alpha_pixels = 0x00000001,
+  alpha = 0x00000002,
+  four_cc = 0x00000004,
+  palette_indexed4 = 0x00000008,
+  palette_indexed8 = 0x00000020,
+  rgb = 0x00000040,
+  luminance = 0x00020000,
+  bump_dudv = 0x00080000,
+};
+
+enum class DDSCaps {
+  alpha = 0x00000002,
+  complex = 0x00000008,
+  texture = 0x00001000,
+  mip_map = 0x00400000,
+};
+
+enum class DDSCaps2 {
+  cubemap = 0x00000200,
+  cubemap_positive_x = 0x00000400,
+  cubemap_negative_x = 0x00000800,
+  cubemap_positive_y = 0x00001000,
+  cubemap_negative_y = 0x00002000,
+  cubemap_positive_z = 0x00004000,
+  cubemap_negative_z = 0x00008000,
+  volume = 0x00400000,
+};
+
+enum class FourCC {
+  DXT1 = 0x44585431,  // DXT1
+  DXT2 = 0x44585432,  // DXT2
+  DXT3 = 0x44585433,  // DXT3
+  DXT4 = 0x44585434,  // DXT4
+  DXT5 = 0x44585435,  // DXT5
+  DX10 = 0x30315844,  // DX10
+  ATI1 = 0x31495441,  // ATI1
+  A16B16G16R16 = 0x00000024,
+  Q16W16V16U16 = 0x0000006E,
+  R16F = 0x0000006F,
+  G16R16F = 0x00000070,
+  A16B16G16R16F = 0x00000071,
+  R32F = 0x00000072,
+  G32R32F = 0x00000073,
+  A32B32G32R32F = 0x00000074,
+  CxV8U8 = 0x00000075,
+  Q8W8V8V8 = 0x0000003f,
+};
+
+std::string to_string(FourCC fourCC);
+
+struct DDSHeader {
+  uint8_t magic[4];
   uint32_t size;
-  std::bitset<32> flags;
+  uint32_t flags;
   uint32_t height;
   uint32_t width;
   uint32_t pitch_or_linear_size;
@@ -19,7 +85,7 @@ struct DDSHeader{
   struct {
     uint32_t size;
     uint32_t flags;
-    uint32_t fourCC;
+    FourCC fourCC;
     uint32_t rgb_bit_count;
     uint32_t r_bit_mask;
     uint32_t g_bit_mask;
@@ -30,7 +96,30 @@ struct DDSHeader{
   uint32_t reserved2;
 };
 
-DDSHeader parse_dds_header(const std::array<uint8_t, 128>& data);
+using DXGIFormat = uint32_t;
+
+enum class DDSResourceDimension {
+  unknown = 0,
+  buffer = 1,
+  texture1d = 2,
+  texture2d = 3,
+  texture3d = 4,
+};
+
+struct DDSHeaderDXT10 {
+  DXGIFormat dxgi_format;
+  DDSResourceDimension resource_dimension;
+  uint32_t misc_flag;
+  uint32_t array_size;
+  uint32_t misc_flags2;
+};
+
+DDSHeader parse_dds_header(const std::array<uint8_t, sizeof(DDSHeader)>& data);
+
+bool is_dx10(const DDSHeader& header);
+
+DDSHeaderDXT10 parse_dds_header_dx10(
+    const std::array<uint8_t, sizeof(DDSHeaderDXT10)>& data);
 
 std::string dump_raw_dds_header(DDSHeader header);
 
@@ -45,7 +134,7 @@ struct DDSData {
 
 RGBA8888 fromRGB565(unsigned short rgb565);
 
-RGBA8888 Lerp(RGBA8888 x, RGBA8888 y, float s) ;
+RGBA8888 Lerp(RGBA8888 x, RGBA8888 y, float s);
 
 DDSData load(const std::filesystem::path& path);
 }  // namespace SF::DDSLoader
